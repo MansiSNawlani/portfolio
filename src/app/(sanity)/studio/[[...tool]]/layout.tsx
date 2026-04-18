ca4669bd4eb1e150b355c83d20b52e03c8cbc502
MansiSNawlani <mansishankarnawlani@gmail.com>
2026-04-19 01:10:44 +0530

    changes for backend content management setup

---
+++ src/app/(sanity)/studio/[[...tool]]/layout.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "NextGen Portfolio Studio",
+  description: "NextGen Portfolio Studio",
+};
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
+
+export default Layout;
