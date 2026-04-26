a150b8dc1425c73386140236bab4b95456579278
MansiSNawlani <mansishankarnawlani@gmail.com>
2026-04-26 18:15:33 +0530

    changes for live editing setup and configuration

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,9 +4,10 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\studio\[[...tool]]\page.tsx` route
  */
 
-import { RocketIcon } from "@sanity/icons";
+import { PresentationIcon, RocketIcon } from "@sanity/icons";
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
+import { presentationTool } from "sanity/presentation";
 import { structureTool } from "sanity/structure";
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./src/sanity/env";
@@ -23,10 +24,25 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-    structureTool({ structure, title: "Content" }),
+    structureTool({
+      structure,
+      title: "Content",
+    }),
+    presentationTool({
+      previewUrl: {
+        initial: process.env.SANITY_STUDIO_PREVIEW_ORIGIN,
+        preview: "/",
+        previewMode: {
+          enable: "/api/draft-mode/enable",
+        },
+      },
+    }),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({ defaultApiVersion: apiVersion, title: "GROQ" }),
+    visionTool({
+      defaultApiVersion: apiVersion,
+      title: "GROQ",
+    }),
   ],
   typegen: {
     path: "./sanity.types.ts",
--- src/app/(portfolio)/layout.tsx
+++ src/app/(portfolio)/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import { draftMode } from "next/headers";
 import { ClerkProvider } from "@clerk/nextjs";
 import { SanityLive } from "@/sanity/lib/live";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
-// import { ModeToggle } from "@/components/DarkModeToggle";
+import { ModeToggle } from "@/components/DarkModeToggle";
 import SidebarToggle from "@/components/SidebarToggle";
 import { FloatingDock } from "@/components/FloatingDock";
 import Script from "next/script";
+import { draftMode } from "next/headers";
+import { DisableDraftMode } from "@/components/DisableDraftMode";
+import { VisualEditing } from "next-sanity/visual-editing";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +28,7 @@ export const metadata: Metadata = {
   description: "NextGen Portfolio",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -50,12 +52,19 @@ export default function RootLayout({
             {/* Mode Toggle - Desktop: bottom right next to AI chat, Mobile: top right next to burger menu */}
             <div className="fixed md:bottom-6 md:right-24 top-4 right-18 md:top-auto md:left-auto z-20">
               <div className="w-10 h-10 md:w-12 md:h-12">
-                {/* <ModeToggle /> */}
+                <ModeToggle />
               </div>
             </div>
           </SidebarProvider>
           {/* Live content API */}
           <SanityLive />
+
+          {(await draftMode()).isEnabled && (
+            <>
+              <VisualEditing />
+              <DisableDraftMode />
+            </>
+          )}
         </body>
       </html>
     </ClerkProvider>
